8b36af9ef77a6c2eeb7cfd7442e92edea0658338
Jhonatan Jaimes <jejo.arch@gmail.com>
2025-07-04 23:02:46 -0500

    Page historial

---
+++ src/components/ddbb/DataBase.jsx
@@ -0,0 +1,17 @@
+class DataBase {
+  constructor() {
+    this.ddbb = [];
+  }
+
+  addItem(element) {
+    this.ddbb.push(element);
+  }
+
+  getItems() {
+    return this.ddbb;
+  }
+
+  removeItem(id) {
+    this.ddbb = this.ddbb.filter((element) => element.id !== id);
+  }
+}
---
+++ src/pages/page-historial/PageHistorial.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageHistorial = () => {
+  return <div></div>;
+};
+
+export default PageHistorial;
